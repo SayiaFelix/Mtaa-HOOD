487afa647e870e25c709bed055f26a2510918a5e
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-18 08:25:57 +0300

    adding homepage views

--- mtaaHud/views.py
+++ mtaaHud/views.py
@@ -11,6 +11,18 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def homepage(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+
+        try:
+          profile = Profile.objects.get(user=current_user)
+        except Profile.DoesNotExist:
+          profile = None
+        # profile = Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('')
 
     return render(request,"Hood/homepage.html")
 
