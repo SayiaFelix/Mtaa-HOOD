2874460f701f2d09a7bf8d8a0eeaca617bfcd38e
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-19 21:00:10 +0300

    adding readme

--- neibahud/settings.py
+++ neibahud/settings.py
@@ -116,13 +116,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jaysafumsela@gmail.com'
-EMAIL_HOST_PASSWORD = 'tmyierwpdfxqhzue'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
